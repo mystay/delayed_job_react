95bc8f580c47f0cc9d59b2b548c4e390cf384c4c
Justin Manche <justin_manche@hotmail.com>
2018-07-06 07:07:05 +1000

    Only run job hooks if ActionCable defined

--- app/services/delayed_job_react/job_hook.rb
+++ app/services/delayed_job_react/job_hook.rb
@@ -3,15 +3,17 @@
 module DelayedJobReact
   class JobHook < Delayed::Plugin
     callbacks do |lifecycle|
-      lifecycle.after(:enqueue) do |_job|
-        ActionCable.server.broadcast 'delayed_job_react_jobs_channel',
-                                     total: Delayed::Job.count,
-                                     failed: Delayed::Job.where('attempts > 2').count
-      end
-      lifecycle.after(:perform) do |_job|
-        ActionCable.server.broadcast 'delayed_job_react_jobs_channel',
-                                     total: Delayed::Job.count,
-                                     failed: Delayed::Job.where('attempts > 2').count
+      if defined?(ActionCable)
+        lifecycle.after(:enqueue) do |_job|
+          ActionCable.server.broadcast 'delayed_job_react_jobs_channel',
+                                       total: Delayed::Job.count,
+                                       failed: Delayed::Job.where('attempts > 2').count
+        end
+        lifecycle.after(:perform) do |_job|
+          ActionCable.server.broadcast 'delayed_job_react_jobs_channel',
+                                       total: Delayed::Job.count,
+                                       failed: Delayed::Job.where('attempts > 2').count
+        end
       end
     end
   end
